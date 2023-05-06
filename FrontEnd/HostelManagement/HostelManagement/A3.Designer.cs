808b71e622c990f856a901831f1196b0e0bca520
bhavjaiin <bhavj20@gmail.com>
2023-05-06 21:29:04 +0530

    BHAVYA CHANGE A3

--- FrontEnd/HostelManagement/HostelManagement/A3.Designer.cs
+++ FrontEnd/HostelManagement/HostelManagement/A3.Designer.cs
@@ -36,6 +36,7 @@
             this.roomchangeicon = new System.Windows.Forms.PictureBox();
             this.messchangeicon = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.profileicon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -49,9 +50,10 @@
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.BackgroundImage = global::HostelManagement.Properties.Resources.stats_icon;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(77, 552);
+            this.pictureBox2.Location = new System.Drawing.Point(58, 448);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(81, 84);
+            this.pictureBox2.Size = new System.Drawing.Size(61, 68);
             this.pictureBox2.TabIndex = 87;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -61,9 +63,10 @@
             this.profileicon.BackColor = System.Drawing.Color.Transparent;
             this.profileicon.BackgroundImage = global::HostelManagement.Properties.Resources.profile_icon;
             this.profileicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.profileicon.Location = new System.Drawing.Point(90, 98);
+            this.profileicon.Location = new System.Drawing.Point(68, 80);
+            this.profileicon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.profileicon.Name = "profileicon";
-            this.profileicon.Size = new System.Drawing.Size(61, 58);
+            this.profileicon.Size = new System.Drawing.Size(46, 47);
             this.profileicon.TabIndex = 86;
             this.profileicon.TabStop = false;
             this.profileicon.Click += new System.EventHandler(this.profileicon_Click);
@@ -71,9 +74,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox1.Location = new System.Drawing.Point(83, 478);
+            this.pictureBox1.Location = new System.Drawing.Point(62, 388);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(72, 5);
+            this.pictureBox1.Size = new System.Drawing.Size(54, 4);
             this.pictureBox1.TabIndex = 85;
             this.pictureBox1.TabStop = false;
             // 
@@ -82,9 +86,10 @@
             this.issuelabel.AutoSize = true;
             this.issuelabel.BackColor = System.Drawing.Color.Transparent;
             this.issuelabel.Font = new System.Drawing.Font("Bookman Old Style", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.issuelabel.Location = new System.Drawing.Point(73, 392);
+            this.issuelabel.Location = new System.Drawing.Point(55, 318);
+            this.issuelabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.issuelabel.Name = "issuelabel";
-            this.issuelabel.Size = new System.Drawing.Size(101, 19);
+            this.issuelabel.Size = new System.Drawing.Size(81, 15);
             this.issuelabel.TabIndex = 84;
             this.issuelabel.Text = "Mess Change";
             // 
@@ -93,9 +98,10 @@
             this.roomchangeicon.BackColor = System.Drawing.Color.Transparent;
             this.roomchangeicon.BackgroundImage = global::HostelManagement.Properties.Resources.roomchange_icon;
             this.roomchangeicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.roomchangeicon.Location = new System.Drawing.Point(90, 262);
+            this.roomchangeicon.Location = new System.Drawing.Point(68, 213);
+            this.roomchangeicon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.roomchangeicon.Name = "roomchangeicon";
-            this.roomchangeicon.Size = new System.Drawing.Size(61, 58);
+            this.roomchangeicon.Size = new System.Drawing.Size(46, 47);
             this.roomchangeicon.TabIndex = 83;
             this.roomchangeicon.TabStop = false;
             this.roomchangeicon.Click += new System.EventHandler(this.roomchangeicon_Click);
@@ -105,9 +111,10 @@
             this.messchangeicon.BackColor = System.Drawing.Color.Transparent;
             this.messchangeicon.BackgroundImage = global::HostelManagement.Properties.Resources.messchange_icon;
             this.messchangeicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.messchangeicon.Location = new System.Drawing.Point(90, 414);
+            this.messchangeicon.Location = new System.Drawing.Point(68, 336);
+            this.messchangeicon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.messchangeicon.Name = "messchangeicon";
-            this.messchangeicon.Size = new System.Drawing.Size(61, 58);
+            this.messchangeicon.Size = new System.Drawing.Size(46, 47);
             this.messchangeicon.TabIndex = 82;
             this.messchangeicon.TabStop = false;
             this.messchangeicon.Click += new System.EventHandler(this.messchangeicon_Click);
@@ -116,21 +123,31 @@
             // 
             this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(471, 73);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Location = new System.Drawing.Point(353, 59);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(284, 167);
+            this.pictureBox6.Size = new System.Drawing.Size(213, 136);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 88;
             this.pictureBox6.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 89;
+            this.label1.Text = "NAME:";
+            // 
             // MessAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::HostelManagement.Properties.Resources.adminmesschange;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1039, 745);
+            this.ClientSize = new System.Drawing.Size(779, 605);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.profileicon);
@@ -139,6 +156,7 @@
             this.Controls.Add(this.roomchangeicon);
             this.Controls.Add(this.messchangeicon);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MessAdmin";
             this.Text = "A3";
             this.Load += new System.EventHandler(this.MessAdmin_Load);
@@ -162,5 +180,6 @@
         private System.Windows.Forms.PictureBox roomchangeicon;
         private System.Windows.Forms.PictureBox messchangeicon;
         private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
     }
 }
