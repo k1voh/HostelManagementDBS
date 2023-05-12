3efbcd25bc8f6524dd591b427be50695b10a97e9
Rishabh Jain <96829987+RampageousRJ@users.noreply.github.com>
2023-05-12 22:22:52 +0530

    Update procedures.sql

--- BackEnd/procedures.sql
+++ BackEnd/procedures.sql
@@ -1,12 +1,3 @@
-create or replace procedure room_option(cgpa IN float)
-is 
-n varchar(10);
-begin
-select driver_id into n from person where person.name=namee;
-dbms_output.put_line(n);
-end;
-/
-
 create or replace procedure fresherCG(sem IN int)
 is
     n int;
